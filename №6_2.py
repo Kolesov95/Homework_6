1ba35e0e5f13239ccb3c8f3844cdc60b4c4d8364
Andrey <pranehs@mail.ru>
2020-09-13 01:39:59 +0300

    Домашнее задание № 6.2

--- №6_2.py
+++ №6_2.py
@@ -1,7 +1,7 @@
 class Road:
-    def __init__(self, _length, _width):
-        self._length = _length
-        self._width = _width
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
 
     def asphalt_weight(self, weigh, thickness):
         return self._length * self._width * weigh * thickness / 1000
